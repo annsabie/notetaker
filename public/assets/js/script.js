168b6724ac2806956b5e06326ffb42f69d5f3a1c
Ann Sabie <annsabie1@gmail.com>
2021-10-21 16:36:41 -0500

    switching to routes folders

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,22 +1,46 @@
 const express = require("express");
-const path = require("path");
+const fs = require("fs");
+const path = require("path")
+const PORT = process.env.PORT || 3001;
+const app = express();
+const notes = require("./db.json");
 
-const PORT = 3001;
-
-const app = express()
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes/html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 app.get("/api/notes", (req, res) => {
-    res.json()
-})
+    fs.readFile(path.join(__dirname, "./db.json"), (err,data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      res.json(notes);
+    })
+});
 
 app.post("/api/notes", (req,res) => {
-  
-})
+    fs.readFile(path.join(__dirname, "./db.json"), (err, data) => {
+      if (err) throw err;
+      const notes = JSON.parse(data);
+      const newNote = req.body;
+      newNote.id = uuid.v4();
+      notes.push(newNote);
+
+      const createNote = JSON.stringify(notes);
+      fs.writeFile(path.join(__dirname, "./db.json"), createNote, (err) => {
+        if (err) throw err;
+      });
+      res.json(newNote);
+    });
+});
+
+app.listen(PORT, () =>
+console.log("App listening at http://localhost:${PORT}")
+);
